4eefb800b8d007a9e31ec9bb254715b537ae26ff
maxim4133 <maxim71515@gmail.com>
2018-03-04 16:28:32 +0300

    Рефакторинг

--- MatrixCode/MatrixCode/IMatrix.cs
+++ MatrixCode/MatrixCode/IMatrix.cs
@@ -4,12 +4,12 @@ namespace MatrixTask
 {
     public interface IMatrix
     {
-        int[][] GetMatrix();
-        void Insert(int i, int j, int value);
         void Delete(int i, int j);
-        List<int> GetListOfMinimaInColumns();
         int GetDiagonalElementsSum();
-        void Transpose();
+        List<int> GetListOfMinimaInColumns();
+        int[][] GetMatrix();
         void GetTwoColumnsSum(int j1, int j2);
+        void Insert(int i, int j, int value);
+        void Transpose();
     }
 }
--- MatrixCode/MatrixCode/Matrix.cs
+++ MatrixCode/MatrixCode/Matrix.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace MatrixTask
 {
-    public class Matrix : IMatrix, IEnumerable
+    public class Matrix : IMatrix, IEnumerable<int>
     {
         private MatrixElement _first;
         private readonly int _size;
@@ -17,55 +17,6 @@ namespace MatrixTask
             for (int i = 0; i < matrix.Length; i++)
                 for (int j = 0; j < matrix[i].Length; j++)
                     InternalInsert(i, j, matrix[i][j]);
-        }
-
-        private void CheckForFirstElement(int lineIndex, int columnIndex, int value)
-        {
-            if (_first == null)
-            {
-                var startElement = new MatrixElement(lineIndex, columnIndex, value);
-                _first = startElement;
-                return;
-            }
-        }
-
-        private void InternalInsert(int lineIndex,int columnIndex,int value)
-        {
-            CheckForFirstElement(lineIndex,columnIndex,value);
-
-            var tempElement = _first;
-
-            while (tempElement.Line * _size + tempElement.Column < lineIndex * _size + columnIndex - 1)
-                if (tempElement.NextItem != null)
-                    tempElement = tempElement.NextItem;
-
-            tempElement.NextItem = new MatrixElement(lineIndex, columnIndex, value);
-        }
-
-        public void GetTwoColumnsSum(int column1, int column2)
-        {
-            throw new NotImplementedException();
-        }
-
-        public int[][] GetMatrix()
-        {
-            int[][] originalMatrix = new int[_size][];
-            var execElement = _first;
-
-            for (int i = 0; i < _size; i++)
-                for (int j = 0; j < _size; j++)
-                {
-                    if (originalMatrix[i] == null)
-                        originalMatrix[i] = new int[_size];
-                    if (execElement.Column == j && execElement.Line == i)
-                    {
-                        originalMatrix[i][j] = execElement.Value;
-                        execElement = execElement.NextItem;
-                    }
-                    else originalMatrix[i][j] = 0;
-                }
-
-            return originalMatrix;
         }
 
         public void Delete(int lineIndex, int columnIndex)
@@ -93,80 +44,6 @@ namespace MatrixTask
             }
         }
 
-        public int GetDiagonalElementsSum()
-        {
-            int sum = 0;
-            var tempElement = _first;
-
-            while(tempElement!=null)
-            {
-                if (tempElement.Line == tempElement.Column || tempElement.Line + tempElement.Column == _size - 1)
-                    sum += tempElement.Value;
-                tempElement = tempElement.NextItem;
-            }
-
-            return sum;
-        }
-
-        private void CheckArgument(int lineIndex, int columnIndex)
-        {
-            if (lineIndex > _size || lineIndex < 0) throw new ArgumentOutOfRangeException();
-            if (columnIndex > _size || columnIndex < 0) throw new ArgumentOutOfRangeException();
-        }
-
-        public void Insert(int lineIndex, int columnIndex, int value)
-        {
-            CheckArgument(lineIndex,columnIndex);
-
-            if (value == 0) 
-            {
-                Delete(lineIndex,columnIndex);
-                return;
-            }
-
-            CheckForFirstElement(lineIndex,columnIndex,value);
-
-            var tempElement = _first;
-
-            while (tempElement.Line * _size + tempElement.Column < lineIndex * _size + columnIndex - 1)
-                if (tempElement.NextItem != null)
-                    tempElement = tempElement.NextItem;
-
-            if (tempElement.NextItem == null)
-                tempElement.NextItem = new MatrixElement(lineIndex, columnIndex, value);
-            else
-                tempElement.NextItem.Value = value;
-        }
-
-        public List<int> GetListOfMinimaInColumns()
-        {
-            List<int> result = new List<int>();
-            int line = 0;
-            int currMin = Int16.MaxValue;
-            var tempElement = _first;
-
-            while(tempElement.NextItem!=null)
-            {
-                if (line < tempElement.Line)
-                {
-                    result.Add(currMin);
-                    currMin = Int16.MaxValue;
-                }
-
-                if (tempElement.Value < currMin)
-                    currMin = tempElement.Value;
-
-                tempElement = tempElement.NextItem;
-            }
-
-            return result;
-        }
-
-        public void Transpose()
-        {
-            throw new NotImplementedException();
-        }
-
         public override bool Equals(object obj)
         {
             var expected = obj as Matrix;
@@ -184,6 +61,135 @@ namespace MatrixTask
             }
 
             return true;
+        }
+
+        public int GetDiagonalElementsSum()
+        {
+            int sum = 0;
+            var tempElement = _first;
+
+            while (tempElement != null)
+            {
+                if (tempElement.Line == tempElement.Column ||
+										tempElement.Line + tempElement.Column == _size - 1)
+                    sum += tempElement.Value;
+                tempElement = tempElement.NextItem;
+            }
+
+            return sum;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public IEnumerator<int> GetEnumerator()
+        {
+            var element = _first;
+
+            while (true)
+            {
+                yield return element.Value;
+                if (element.NextItem != null)
+                    element = element.NextItem;
+                else
+                    yield break;
+            }
+        }
+
+        public List<int> GetListOfMinimaInColumns()
+        {
+            List<int> result = new List<int>();
+            int line = 0;
+            int currMin = Int16.MaxValue;
+            var tempElement = _first;
+
+            while (tempElement.NextItem != null)
+            {
+                if (line < tempElement.Line)
+                {
+                    result.Add(currMin);
+                    currMin = Int16.MaxValue;
+                }
+
+                if (tempElement.Value < currMin)
+                    currMin = tempElement.Value;
+
+                tempElement = tempElement.NextItem;
+            }
+
+            return result;
+        }
+
+        public int[][] GetMatrix()
+        {
+            int[][] originalMatrix = new int[_size][];
+            var execElement = _first;
+
+            for (int i = 0; i < _size; i++)
+            {
+                originalMatrix[i] = new int[_size];
+                for (int j = 0; j < _size; j++)
+                {
+                    if (execElement.Column == j && execElement.Line == i)
+                    {
+                        originalMatrix[i][j] = execElement.Value;
+                        execElement = execElement.NextItem;
+                    }
+                    else
+                        originalMatrix[i][j] = 0;
+                }
+            }
+
+            return originalMatrix;
+        }
+
+        public void GetTwoColumnsSum(int column1, int column2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int lineIndex, int columnIndex, int value)
+        {
+            CheckArgument(lineIndex, columnIndex);
+
+            if (value == 0) 
+            {
+                Delete(lineIndex,columnIndex);
+                return;
+            }
+
+            CheckForFirstElement(lineIndex, columnIndex, value);
+
+            var tempElement = _first;
+
+            while (tempElement.Line * _size + tempElement.Column < lineIndex * _size + columnIndex - 1)
+                if (tempElement.NextItem != null)
+                    tempElement = tempElement.NextItem;
+
+            if (tempElement.NextItem == null)
+                tempElement.NextItem = new MatrixElement(lineIndex, columnIndex, value);
+            else
+                tempElement.NextItem.Value = value;
+        }
+
+        public void Transpose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int this[int lineIndex, int columnIndex]
+        {
+            get
+            {
+                CheckArgument(lineIndex, columnIndex);
+
+                var tempElement = _first;
+                while (tempElement.Line * _size + tempElement.Column < lineIndex * _size + columnIndex)
+                    if (tempElement.NextItem != null)
+                        tempElement = tempElement.NextItem;
+
+                if (tempElement.Line != lineIndex || tempElement.Column != columnIndex)
+                    throw new InvalidOperationException();
+                return tempElement.Value;
+            }
         }
 
         private sealed class MatrixElement
@@ -209,36 +215,35 @@ namespace MatrixTask
             public int Value { get; set; }
         }
 
-        public IEnumerator GetEnumerator()
+        private void CheckArgument(int lineIndex, int columnIndex)
         {
-            var element = _first;
-
-            while (true)
+            if (lineIndex >= _size || lineIndex < 0)
+                throw new ArgumentOutOfRangeException("lineIndex");
+            if (columnIndex >= _size || columnIndex < 0)
+                throw new ArgumentOutOfRangeException("columnIndex");
+        }
+	        
+        private void CheckForFirstElement(int lineIndex, int columnIndex, int value)
+        {
+            if (_first == null)
             {
-                yield return element;
-                if (element.NextItem != null)
-                    element = element.NextItem;
-                else yield break;
+                var startElement = new MatrixElement(lineIndex, columnIndex, value);
+                _first = startElement;
+                return;
             }
         }
 
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-        public int this[int lineIndex, int columnIndex]
+        private void InternalInsert(int lineIndex,int columnIndex,int value)
         {
-            get
-            {
-                CheckArgument(lineIndex, columnIndex);
+            CheckForFirstElement(lineIndex,columnIndex,value);
 
-                var tempElement = _first;
-                while (tempElement.Line * _size + tempElement.Column < lineIndex * _size + columnIndex)
-                    if (tempElement.NextItem != null)
-                        tempElement = tempElement.NextItem;
+            var tempElement = _first;
 
-                if (tempElement.Line != lineIndex || tempElement.Column != columnIndex)
-                    throw new InvalidOperationException();
-                return tempElement.Value;
-            }
+            while (tempElement.Line * _size + tempElement.Column < lineIndex * _size + columnIndex - 1)
+                if (tempElement.NextItem != null)
+                    tempElement = tempElement.NextItem;
+
+            tempElement.NextItem = new MatrixElement(lineIndex, columnIndex, value);
         }
     }
 }
